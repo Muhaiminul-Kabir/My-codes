bc47ba49d215b7aca1be272494c76bd0803060cc
Muhaiminul-Kabir <muhaiminulkabir32@gmail.com>
2021-06-09 20:40:16 +0600

    new

--- Physics_projectile.cpp
+++ Physics_projectile.cpp
@@ -2,27 +2,22 @@
 #include<math.h>
 using namespace std;
 
-
-float getDis(float theta , float v0){
-  float dis;
-  dis = (v0*v0*sin(2*theta))/g ;
-  return dis;
-}
-
 float g = 9.8f ;
 float u = 15;
-
 float c = 10 * (3.1416/180);
-int main()
-{
-    float x = 0.0f ,y;
-    //y = tan(c)*x ;
-    for(int i = 0;i< getDis(c,u) ; i++){
-        x++;
-       
-        
-         y = tan(c)*x - ((g*x*x)/((2*u*cos(c))*(2*u*cos(c))));
-         
+
+float getProjectileDis(float theta, float v0) {
+    float dis;
+    dis = (v0*v0*sin(2*theta))/g ;
+    return dis;
+}
+
+
+int main() {
+    float x = 0.0f,y;  //y = tan(c)*x ;
+    for(int i = 0; y>0.0 ; i++) {
+        x+=0.1;
+        y = tan(c)*x - ((g*x*x)/((2*u*cos(c))*(2*u*cos(c))));
         cout <<x<<' '<<y<<endl;
     }
     return 0;
