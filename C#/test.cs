25c7a2eaaf8f70a002286aa8f138e43ca531c928
Muhaiminul-Kabir <75292558+Muhaiminul-Kabir@users.noreply.github.com>
2021-06-16 00:10:27 +0600

    Create test.cs

---
+++ C#/test.cs
@@ -0,0 +1,9 @@
+using System;
+namespace Hello{
+    class Main{
+        static void main(string[] args){
+            Console.Writeln("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
