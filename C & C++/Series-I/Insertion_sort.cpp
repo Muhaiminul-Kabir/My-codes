afdcc50ef16029bb1cae642663763d7ba3e36171
Muhaiminul-Kabir <75292558+Muhaiminul-Kabir@users.noreply.github.com>
2021-07-05 11:18:04 +0600

    Create Insertion_sort.cpp

---
+++ C & C++/Series-I/Insertion_sort.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<time.h>
+#include<stdlib.h>
+using namespace std;
+int i= 0,j =0;
+int c = 0;
+double cpuTime(int x[],int size,void (*p)(int*,int)) {
+    clock_t t;
+    t = clock();
+    p(x,size);
+    t = clock()-t;
+    double time_taken = (double)t/CLOCKS_PER_SEC ;
+    return time_taken ;
+}
+
+bool isSort(int x[],int size) {
+    for (j = 0; j < size - 1; j++) {
+        if(x[j] > x[j + 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+void sortArr(int x[],int size) {
+    cout<<boolalpha<<isSort(x,size);
+    for (; i < size; i++) {
+        if(!isSort(x,size)) {
+            for (int step = 1; step < size; step++) {
+                int key = x[step];
+                int j = step - 1;
+                while (key < x[j] && j >= 0) {
+                    x[j + 1] = x[j];
+                    --j;
+                }
+                x[j + 1] = key;
+            }
+        }
+        else break;
+    }
+}
+int main() {
+    void (*p)(int*,int);
+    int x[] = {1,2,3,1,4,5,4};
+    int size = sizeof(x) / sizeof(x[0]);
+    p = &sortArr;
+    double time = cpuTime(x,size,*p);
+    cout<<endl;
+    for (i = 0; i < size; i++) {
+        cout << x[i] << " ";
+    }
+    cout<<endl<<"Program executed in "<<time<<"s";
+}
