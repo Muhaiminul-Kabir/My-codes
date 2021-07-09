32a692cae403be8eedadf3b51efcddafe8fe8511
Muhaiminul-Kabir <75292558+Muhaiminul-Kabir@users.noreply.github.com>
2021-07-09 15:11:33 +0600

    Create linklist_init.c

---
+++ C & C++/Series-I/linklist_init.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define HEAD arr[0]
+
+struct node {
+    int data;
+    struct node *next;
+
+};
+typedef struct node node;
+
+void printLinkedlist(struct node *p) {
+    while (p != NULL) {
+        printf(" %d ", p->data);
+        p = p->next;
+    }
+}
+
+int main()
+{
+    int y = 4,i =0;
+    node *arr[y] ;//array of node pointers
+
+
+    for( i = 0; i< 4; i++) {
+        arr[i] = NULL;
+        if(i != 0) {
+            arr[i] = (node*)malloc(sizeof(node));
+
+
+        }
+
+    }
+    int x = 1;//skipping arr[0] AKA head
+    while(x< 4) {
+        arr[x]->data = x;
+
+        x++;
+    }
+    int r= 1;//skipping arr[0] AKA head
+    while(r< 3/*last node's linking address will be NULL*/) {
+        arr[r]->next = arr[r + 1];
+
+        r++;
+    }
+    arr[r] = NULL;
+    HEAD = arr[1];//setting arr[1] as head
+
+
+
+
+    printf("[");
+    printLinkedlist(HEAD);
+    printf("]");
+    return 0;
+}
