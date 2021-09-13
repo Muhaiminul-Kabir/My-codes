51bee4f76d88e6de23b0b748cef44111fa721c83
Muhaiminul-Kabir <75292558+Muhaiminul-Kabir@users.noreply.github.com>
2021-09-13 10:36:29 +0600

    Create DFS.cpp

---
+++ Online/DFS.cpp
@@ -0,0 +1,192 @@
+#include<iostream>
+
+
+#define null NULL
+
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* next;
+    Node(int data, struct Node* next) {
+        this->data = data;
+        this->next = next;
+    }
+};
+
+typedef struct Node* node;
+
+node head = null;
+node rear = null;
+node tail = null;
+node trash = null;
+
+
+node create_node(int data) {
+    node new_node = null;
+    // new_node = (node)malloc(sizeof(struct Node));
+    new_node = new Node(data,null);
+    if(head == NULL) {
+        head = new_node;
+
+    } else {
+        node p = head;
+        while(p->next != NULL) {
+            p = p->next;
+        }
+        p->next = new_node;
+    }
+    return head;
+
+}
+
+
+void del_FIFO() {
+
+    node temp = head;
+    if(head != null) {
+        head = temp ->next;
+
+    }
+    else {
+        cout<<endl<<"underflow";
+    }
+
+
+}
+
+
+
+void print(node x) {
+    node current_node = x;
+    if(current_node == NULL) {
+        cout<<endl<<"null";
+    }
+    while ( current_node != NULL) {
+        printf("%d ", current_node->data);
+        current_node = current_node->next;
+    }
+
+}
+
+node get_rear() {
+
+    node temp = head;
+    while(temp -> next!= null) {
+        temp = temp->next;
+
+    }
+    return temp;
+
+}
+
+void del_LIFO() {
+
+    node temp = head;
+
+    if(temp ==  null) {
+        cout<<endl<<"underflow";
+        return ;
+    }
+
+    if(temp->next == null) {
+        trash = temp ;
+        head = null;
+        cout<<endl<<"empty"<<endl;
+    }
+    while(temp->next != null) {
+
+
+        if(temp->next == rear) {
+            // cout<<endl<<"hi";
+            trash = temp->next;
+           // cout<<trash->next;
+            temp->next = null;
+            rear = temp;
+            break;
+        }
+        temp = temp->next;
+
+    }
+
+}
+
+
+bool emp(node head){
+    return head==null;
+
+}
+
+
+int graph[200][200]={0};
+int i= 0;
+int j=0;
+int x,y;
+
+void graphs(){
+string t ;
+    for(;;){
+        cout<<"vertices ";
+        cin>>x;
+        cout<<" edge";
+        cin>>y;
+        cin>>t;
+        graph[x][y]=1;
+        graph[y][x]=1;
+        if(t=="t"){
+            
+            break;
+        }
+        
+    }
+    for(i = 0;i<=x;i++){
+        
+        for(j = 0;j<=y;j++){
+            cout<<" "<<graph[i][j]<<" ";
+            
+        }
+        cout<<endl ;
+    }
+
+
+
+}
+
+
+void DFS(int start, bool *vist){
+
+    
+    cout<<start<<endl;
+    
+    vist[start]=true;
+    
+   
+        
+        for(int i = 0; i<=y; i++){
+            if(graph[start][i] == 1 && !(vist[i])){
+            
+                DFS(i,vist);
+            
+            }
+        
+        }
+        
+        
+        
+    
+
+
+
+
+}
+
+
+int main() {
+    graphs();
+    bool vist[x] ={false};
+    
+    
+    DFS(0,vist);
+    
+}
+    
